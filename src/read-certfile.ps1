d098980fc5f12d18db83a914ddb08db3259198ac
Vidar Kongsli <vidarkongsli@yahoo.com>
2014-12-03 22:09:22 +0100

    Can read .pfx

--- src/read-certfile.ps1
+++ src/read-certfile.ps1
@@ -1,3 +1,4 @@
+#requires -version 3
 param (
     [ValidateScript({test-path $_})]
     [Parameter(Mandatory=$true)]
@@ -11,6 +12,8 @@ if ($filetype -eq '.pfx') {
     if (-not($password)) {
         Write-Error "You have to enter password for .pfx file"
     }
+    $cert = get-pfxcertificate -filepath (resolve-path $file)
+} else {
+    $cert = new-object System.Security.Cryptography.X509Certificates.X509Certificate2 -ArgumentList @(,(resolve-path $file), [string]$password, 0)
 }
-$cert = new-object System.Security.Cryptography.X509Certificates.X509Certificate2 -ArgumentList @(,(resolve-path $file),$password, 0)
 $cert
